e17ebc80c9fc6a3374562a4e277244cf21b6b02f
suhaibinsari <mstar390@gmail.com>
2024-03-15 17:57:29 +0500

    GraphQl

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,273 @@
+import { ApolloServer } from '@apollo/server';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+
+// RESTAPIS
+// GET, POST, PUT, DELETE
+
+//GRAPHQL
+// QUERY, MUTATITION, SUBSCRIPTIONS ==> USED IN REAL TIME
+
+
+
+let dummyProducts = [
+    {
+        "id": 1,
+        "title": "iPhone 9",
+        "description": "An apple mobile which is nothing like apple",
+        "price": 549,
+        "discountPercentage": 12.96,
+        "rating": 4.69,
+        "stock": 94,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/1/1.jpg",
+            "https://cdn.dummyjson.com/product-images/1/2.jpg",
+            "https://cdn.dummyjson.com/product-images/1/3.jpg",
+            "https://cdn.dummyjson.com/product-images/1/4.jpg",
+            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 2,
+        "title": "iPhone X",
+        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+        "price": 899,
+        "discountPercentage": 17.94,
+        "rating": 4.44,
+        "stock": 34,
+        "brand": "Apple",
+        "category": "smartphones",
+        "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/2/1.jpg",
+            "https://cdn.dummyjson.com/product-images/2/2.jpg",
+            "https://cdn.dummyjson.com/product-images/2/3.jpg",
+            "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Samsung Universe 9",
+        "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+        "price": 1249,
+        "discountPercentage": 15.46,
+        "rating": 4.09,
+        "stock": 36,
+        "brand": "Samsung",
+        "category": "smartphones",
+        "thumbnail": "https://cdn.dummyjson.com/product-images/3/thumbnail.jpg",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/3/1.jpg"
+        ]
+    },
+    {
+        "id": 4,
+        "title": "OPPOF19",
+        "description": "OPPO F19 is officially announced on April 2021.",
+        "price": 280,
+        "discountPercentage": 17.91,
+        "rating": 4.3,
+        "stock": 123,
+        "brand": "OPPO",
+        "category": "smartphones",
+        "thumbnail": "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/4/1.jpg",
+            "https://cdn.dummyjson.com/product-images/4/2.jpg",
+            "https://cdn.dummyjson.com/product-images/4/3.jpg",
+            "https://cdn.dummyjson.com/product-images/4/4.jpg",
+            "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+        ]
+    },
+    {
+        "id": 5,
+        "title": "Huawei P30",
+        "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+        "price": 499,
+        "discountPercentage": 10.58,
+        "rating": 4.09,
+        "stock": 32,
+        "brand": "Huawei",
+        "category": "smartphones",
+        "thumbnail": "https://cdn.dummyjson.com/product-images/5/thumbnail.jpg",
+        "images": [
+            "https://cdn.dummyjson.com/product-images/5/1.jpg",
+            "https://cdn.dummyjson.com/product-images/5/2.jpg",
+            "https://cdn.dummyjson.com/product-images/5/3.jpg"
+        ]
+    },[
+        {
+            "id": 1,
+            "title": "iPhone 9",
+            "description": "An apple mobile which is nothing like apple",
+            "price": 549,
+            "discountPercentage": 12.96,
+            "rating": 4.69,
+            "stock": 94,
+            "brand": "Apple",
+            "category": "smartphones",
+            "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+            "images": [
+                "https://cdn.dummyjson.com/product-images/1/1.jpg",
+                "https://cdn.dummyjson.com/product-images/1/2.jpg",
+                "https://cdn.dummyjson.com/product-images/1/3.jpg",
+                "https://cdn.dummyjson.com/product-images/1/4.jpg",
+                "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 2,
+            "title": "iPhone X",
+            "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+            "price": 899,
+            "discountPercentage": 17.94,
+            "rating": 4.44,
+            "stock": 34,
+            "brand": "Apple",
+            "category": "smartphones",
+            "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",
+            "images": [
+                "https://cdn.dummyjson.com/product-images/2/1.jpg",
+                "https://cdn.dummyjson.com/product-images/2/2.jpg",
+                "https://cdn.dummyjson.com/product-images/2/3.jpg",
+                "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 3,
+            "title": "Samsung Universe 9",
+            "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+            "price": 1249,
+            "discountPercentage": 15.46,
+            "rating": 4.09,
+            "stock": 36,
+            "brand": "Samsung",
+            "category": "smartphones",
+            "thumbnail": "https://cdn.dummyjson.com/product-images/3/thumbnail.jpg",
+            "images": [
+                "https://cdn.dummyjson.com/product-images/3/1.jpg"
+            ]
+        },
+        {
+            "id": 4,
+            "title": "OPPOF19",
+            "description": "OPPO F19 is officially announced on April 2021.",
+            "price": 280,
+            "discountPercentage": 17.91,
+            "rating": 4.3,
+            "stock": 123,
+            "brand": "OPPO",
+            "category": "smartphones",
+            "thumbnail": "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg",
+            "images": [
+                "https://cdn.dummyjson.com/product-images/4/1.jpg",
+                "https://cdn.dummyjson.com/product-images/4/2.jpg",
+                "https://cdn.dummyjson.com/product-images/4/3.jpg",
+                "https://cdn.dummyjson.com/product-images/4/4.jpg",
+                "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 5,
+            "title": "Huawei P30",
+            "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+            "price": 499,
+            "discountPercentage": 10.58,
+            "rating": 4.09,
+            "stock": 32,
+            "brand": "Huawei",
+            "category": "smartphones",
+            "thumbnail": "https://cdn.dummyjson.com/product-images/5/thumbnail.jpg",
+            "images": [
+                "https://cdn.dummyjson.com/product-images/5/1.jpg",
+                "https://cdn.dummyjson.com/product-images/5/2.jpg",
+                "https://cdn.dummyjson.com/product-images/5/3.jpg"
+            ]
+    },]]
+
+
+const gql = String.raw
+
+const typeDefs = gql`
+
+type Query{
+    getProducts: [product]
+    getUserName: String
+}
+
+
+type Mutation{
+    createProduct(product: productInputType) : product
+    deleteProduct(id: Int) : String
+}
+
+
+input productInputType {
+    id: Int
+    title: String
+    description: String
+    price: Float
+    discountPercentage: Float
+    rating: Float
+    stock: Int
+    brand: String
+    category: String
+    thumbnail: String
+    images: [String]
+}
+
+
+type product{
+    id: Int
+    title: String
+    description: String
+    price: Float
+    discountPercentage: Float
+    rating: Float
+    stock: Int
+    brand: String
+    category: String
+    thumbnail: String
+    images: [String]
+}
+
+
+
+
+`
+
+const resolvers = {
+
+    Query:{
+        getProducts:()=>{
+            return dummyProducts
+        },
+        getUserName:()=>{
+            return 'Moon'
+        }
+    },
+    Mutation:{
+        createProduct:(root:{}, args:{product:{}}, context:{}, info:{})=>{
+            console.log('args',args)
+            dummyProducts.push(args.product)
+            return args.product
+        },
+        deleteProduct:(root:{}, args:{id:Number}, context:{}, info:{})=>{
+            dummyProducts = dummyProducts.filter((product) => product.id !== args.id);
+            return 'Product has been deleted'
+        }
+
+    }
+
+}
+
+const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+  });
+
+const handler = startServerAndCreateNextHandler(server)
+
+export {handler as GET, handler as POST};
